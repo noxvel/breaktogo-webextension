fe1f9ad3be90b952cfb06564678739daf1eb9e77
Станислав Божевский <s.bogevskiy@into-sana.ua>
2019-12-27 13:13:36 +0200

    Change block module

--- content.js
+++ content.js
@@ -2,22 +2,22 @@ chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.message === "block_page") {
       if (request.isBreak || !request.isTimer){
-        $.modal.close();
+        const modal = document.querySelector('#break-to-go-modal-container')
+        if (modal) {
+          modal.remove();
+          $('body').css('overflow', 'auto');
+        }
       }else{
-        if (!$("#break-to-go-block-modal").length){
-          $('body').append(`<div id="break-to-go-block-modal" class="modal">
-                              <h2 style="color: #5D8EE4;">Break To Go</h2>
-                              <h3>Sorry, but you need to wait for a break to view this site.</h3>
-                              <p>You can disable this feature or change the list of sites to block in the settings.</p>
+        if(!document.querySelector('#break-to-go-modal-container')){
+          $('body').append(`<div id="break-to-go-modal-container">
+                                <div id="break-to-go-modal-child">
+                                  <h2>Break To Go</h2>
+                                  <h3>Sorry, but you need to wait for a break to view this site.</h3>
+                                  <p>You can disable this feature or change the list of sites to block in the settings.</p>
+                                </div>
                               </div>`)
                             }
-                            // <a href="#" rel="modal:close">Close</a>
-        $("#break-to-go-block-modal").modal({
-          escapeClose: false,
-          clickClose: false,
-          showClose: false,
-          blockerClass: 'jquery-modal special-blocker-break-to-go',
-        });
+          $('body').css('overflow', 'hidden');
       }
     }
   }
